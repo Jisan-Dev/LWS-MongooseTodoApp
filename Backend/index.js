3bd626c6caf098f260b766dc7c657f1cbe2683d8
Jisun696 <istiakkashem35@gmail.com>
2024-08-21 18:38:43 +0600

    mongodb connection with mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = 3000 || process.env.PORT;
 
 // express app initialization
 const app = express();
 app.use(express.json());
+
+// database connection with mongoose
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
 
 // application routes
 
