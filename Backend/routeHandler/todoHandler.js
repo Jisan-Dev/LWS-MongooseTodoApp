f1eb224690698fc496225dbb1218989d5269568e
Jisun696 <istiakkashem35@gmail.com>
2024-08-21 19:26:40 +0600

    set up todo route handlers and mongoose schema

--- Backend/routeHandler/todoHandler.js
+++ Backend/routeHandler/todoHandler.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const todoSchema = require('../schemas/todoSchema');
+const Todo = mongoose.model('Todo', todoSchema);
 
 // GET ALL THE TODO
 router.get('/', async (req, res) => {});
